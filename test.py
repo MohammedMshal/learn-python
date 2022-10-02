b1923bc87efc7d10b8d7187b1df10b3f95eb4ed0
MohammedMshal <mohammedmshal2018@gmail.com>
2022-10-02 08:27:29 +0200

    add strings

--- test.py
+++ test.py
@@ -5,6 +5,19 @@
 #print('my name is {n} and {t}'.format( n=name,t='test'))
 #print('my name is {1} and {0}'.format(name,'test'))
 ############################################################
+var1 = 'hello'
+name = 'hello my name is Mohammed Mhsal'
+#طباعة او الحصول على حرف معين من المتغير من خلال الاندكس
+print(var1[1])
+#الحصول على اخر حرف فى الكلمة بطريقة عكسية
+print(var1[-1])
+#الحصول على مجموعة معينة من المتغير
+#start:stop:step
+print(name[:8:])
+#طباعة عدد الخانات
+print(len(name))
+print(name.index)
+#دمج النصوص
 num = 33.6546545
 name = 'Mohammed Mshal'
 #one
