505a556ac674ccb382828100d4ddc95a8e296101
MohammedMshal <mohammedmshal2018@gmail.com>
2022-10-02 07:16:38 +0200

    format str in python

---
+++ test.py
@@ -0,0 +1,15 @@
+# print string with var
+# one
+#name = 'Mohammed Mshal'
+#print('my name is ' + name)
+#print('my name is {n} and {t}'.format( n=name,t='test'))
+#print('my name is {1} and {0}'.format(name,'test'))
+############################################################
+num = 33.6546545
+#one
+print('number is {}'.format(num))
+#two
+print('the number is {:10.2f}'.format(num))
+#three
+print(f'the number is {num:10.3f}')
+
