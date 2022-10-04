68510504f5cc79fa94fea1dd0918afeec147d9a6
MohammedMshal <mohammedmshal2018@gmail.com>
2022-10-04 06:04:21 +0200

    read files

--- test.py
+++ test.py
@@ -1,17 +1,32 @@
-#السيت يمكن تخزين بها عدة قيم ولكن لا تدعم التسلسل 
-#ايضا السيت لا يمكن ان يحتوى على قيم مكرره
-mySet = {
-    1,
-    'two',
-    3,
-    'four'
-}
+#يمكن التعامل مع الملفات من خلال هذه الدالة ويتم إنشاء الملف بشكل اتوماتيكى فى موق ع الملف
+myFile = open('C:\\Users\\moham\\Desktop\\learn python\\test.txt','rt')
+#لقراءة الملف
+print(myFile.read())
+#لإعادة بدء الملف الأول
+myFile.seek(0)
+#لقراءة جزء من الملف
+print(myFile.read(4))
+myFile.seek(0)
+#لقراءة الملف كسطر واحد فقد
+print(myFile.readline())
+myFile.seek(0)
+#لقراءة الملف كأسطر
+print(myFile.readlines())
+myFile.seek(0)
+#للحصول على الملف كليست والتعديل عليه
+myLits= []
+for line in myFile:
+    myLits.append(line.strip())
 
-print(mySet)
-#يمكن تحويل السيت الى ليست كالآتى
-myList = [1,3,'three']
-newSet = set(myList)
-print(newSet)
-#يمكن إضافة عنصر  إلى السيت من كالآتى
-mySet.add('six')
-print(mySet)
+print( f'my list is {myLits}')
+myFile.close()
+
+with open('test.txt') as myFile2:
+    data = myFile2.read()
+
+print(f'my file 2 is \n {data}')
+
+
+
+#get path (pwd)
+#open location path (ls)
