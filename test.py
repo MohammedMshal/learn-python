7e1e18da04c926f8873d1d969481c3a1ad20987b
MohammedMshal <mohammedmshal2018@gmail.com>
2022-10-05 16:24:04 +0200

    map_lambda_reduce

--- test.py
+++ test.py
@@ -1,15 +1,50 @@
-#نستعملها عندما نريد وضع عدد لانهائى من البراميترات
-def sumNum(*args):
-    return(sum((args)))
+from functools import reduce
+myList = [1,2,3,4,5,6,7,8,9,10]
+myList_name = ['Mohammed','ahmed','fady','sarah','shimaa','abdo']
+def squre(num):
+    return num ** 2
 
-result_num = sumNum(3,6,2,7,8)
-print(result_num)
+result_num1 = []
 
-#مثال عندما نريد وضع عدد لا نهائى من الراميترات على شكل {مفتاح  , قيمة}
-def myfunc(**kwargs):
-    print(kwargs)
-    for item in kwargs:
-        print(f'{item} is {kwargs[item]}')
+for num in myList:
+    result_num1.append(squre(num))
+print(result_num1)
 
-myfunc(name='mohammed',age=22,brath_day='sptamper')
+result_num2 = []
+for item in map(squre,myList):
+    result_num2.append(item)
+print(result_num2)
 
+result_num3 = list(map(squre,myList))
+print(result_num3)
+#هنا قمنا بإستخدام الماب ولامبادا لاختصار الكود
+result_num4 = list(map(lambda num: num ** 2,myList))
+print(result_num4)
+
+def check_even(num):
+    return num % 2 == 0
+
+for item in myList:
+    print(check_even(item))
+
+#سنقوم بإستخدام الفلتر
+#المثال الأول سنقوم بفلترة الارقام الزوحية
+print(list(filter(lambda num: num % 2 == 0,myList)))
+#المثال الثانى سنقوم بفلترة الاسماء التى تحتوى على خمسة أحرف فقط
+print(list(filter(lambda name: len(name) <= 5, myList_name)))
+
+#reduce
+#هذه بالطريقة العادية
+total=0
+for num in myList:
+    total += num
+print(total)
+#هنا سنستخدم الريديوس
+print(reduce (lambda total,num:total +  num ,myList,0))
+#مثال أخر اولاً سنستخدم ا لطريقة العادية
+mul = 1
+for num in  myList:
+    mul *= num
+print(mul)
+#هنا سنستخدم الريديوس\
+print(reduce(lambda total,num:total * num,myList,1))
